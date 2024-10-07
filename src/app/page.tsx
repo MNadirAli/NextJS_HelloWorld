2b735eb809030948a334de94e83eeaf25efa4e5b
MNadirAli <mnadiralishiekh@gmail.com>
2024-10-07 11:54:50 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div className='h-40 w-800
+    container mx-auto my-96 '><h1 className='bg-slate-400 font-bold text-6xl'> Hello World , This is Nadir</h1></div>
+  );
+}
